2cb54d2e61c9e259b2cdce602e53231e4f090a44
Mybralaoui <alaouimybr@gmail.com>
2026-01-21 09:58:38 +0100

    add h2 tag

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h2>Hello there!</h2>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
